057a8659957eb66b3238766a61bfd57aad01df1f
clvx <michael.ibarra@gmail.com>
2020-08-28 09:54:05 -0600

    terraform config/backend

--- main.tf
+++ main.tf
@@ -1,13 +1,21 @@
 terraform {
-    required_providers {
-        azurerm = {
-            source = "hashicorp/azurerm"
-            version = "~>2.0.0"
-        }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>2.25.0"
     }
+  }
+  backend "azurerm" {
+    resource_group_name  = var.tf_backend_rg
+    storage_account_name = var.tf_backend_storage
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+
 }
+
 
 # Configure the Azure provider
 provider "azurerm" {
-    features {}
+  features {}
 }
